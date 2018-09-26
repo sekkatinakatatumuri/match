0d0f3b7806f997202adc84f6e15c4185610a3c87
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-26 06:53:01 +0000

    add: application.jsファイルにjquery3とpopperとbootstrap-sprocketsを使用するコードを追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// require_tree .で読み込むものは、
+// jquery3などのコードが読み込まれてからでないとエラーが起きる順番も重要
 //= require rails-ujs
 //= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
