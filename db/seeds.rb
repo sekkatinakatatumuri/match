927df1a086d5de609bc3d17eeb260d9810ed56e7
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-30 16:29:46 +0000

    update: seedsファイルのダミーユーザー情報を更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,32 @@ p "create Users"
 User.create!(
   email: 'user1@example.com',
   password: 'aaaaaaaa',
-  name: '佐藤 太郎',
-  self_introduction: '佐藤太郎です',
-  sex: 0,
+  name: '西野 七瀬',
+  self_introduction: '告白の二度聞きは禁止やで！',
+  sex: 1,
   img_name: open("#{Rails.root}/db/dummy_images/1.jpg")
 )
 User.create!(
   email: 'user2@example.com',
   password: 'aaaaaaaa',
-  name: '平野 貴史',
-  self_introduction: '平野貴史です',
-  sex: 0,
+  name: '白石 麻衣',
+  self_introduction: '四次元から来ましたマヨラー星人、19歳、白石麻衣です。',
+  sex: 1,
   img_name: open("#{Rails.root}/db/dummy_images/2.jpg")
 )
 User.create!(
   email: 'user3@example.com',
   password: 'aaaaaaaa',
-  name: '田渕 望',
-  self_introduction: '田渕望です',
+  name: '深川 麻衣',
+  self_introduction: 'まいまい毎日マイペース♪',
   sex: 1,
   img_name: open("#{Rails.root}/db/dummy_images/3.jpg")
 )
 User.create!(
   email: 'user4@example.com',
   password: 'aaaaaaaa',
-  name: '高谷 瑠美',
-  self_introduction: '高谷瑠美です',
+  name: '渡邉 理佐',
+  self_introduction: 'こぼしてんじゃねーよ！',
   sex: 1,
   img_name: open("#{Rails.root}/db/dummy_images/4.jpg")
 )
