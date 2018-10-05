960680ed8c07110d67bc5baa258b7e6ed874c4f0
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-05 06:21:25 +0000

    add: ReactionモデルにUserモデルとのアソシエーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180926072403) do
+ActiveRecord::Schema.define(version: 20181004050357) do
+
+  create_table "reactions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
