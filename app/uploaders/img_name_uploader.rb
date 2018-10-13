af3cd1593a596289819e02f94b63a072bd91ea05
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-13 22:56:40 +0900

    update: Gemファイルのrmagickをmini_magickに変更

--- app/uploaders/img_name_uploader.rb
+++ app/uploaders/img_name_uploader.rb
@@ -1,8 +1,8 @@
 class ImgNameUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
   # リサイズしたり画像形式を変更するのに必要
-  include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181004050357) do
+ActiveRecord::Schema.define(version: 20181010131008) do
+
+  create_table "chat_room_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reactions", force: :cascade do |t|
     t.datetime "created_at", null: false
